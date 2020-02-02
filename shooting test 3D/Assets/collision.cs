096f83a4356ab44e91443002f456fc1e749ddbb8
skyfall1818 <phid8181@hotmail.com>
2020-02-02 17:03:57 -0500

    3D shooting test
    testing how shooting works on unity

---
+++ shooting test 3D/Assets/collision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        double num = 25.8;
+        transform.position = new Vector3((float)num, transform.position.y, transform.position.z);
+        if (other.gameObject.tag == "bullet")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ shooting test 3D/Assets/turnandshoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnandshoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public int gunAngle = 0;
+    public float RotationSpeed;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.up * - (RotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKeyDown("v"))
+        {
+            Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            GameObject bull = Instantiate(bullet, position, transform.rotation) as GameObject;
+            Rigidbody bullrigidbody = bull.GetComponent<Rigidbody>();
+            bullrigidbody.AddForce(transform.right * 2000);
+        }
+    }
+}
