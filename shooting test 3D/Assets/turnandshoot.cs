7b59069b5a01c58e695825f6fcc00a80cc70c28b
skyfall1818 <phid8181@hotmail.com>
2020-02-11 16:59:18 -0500

    added cannon and character model
    arrow keys can now move the cannon and space shoots the bullets. Added a character model from unity's free assests.

--- shooting test 3D/Assets/turnandshoot.cs
+++ shooting test 3D/Assets/turnandshoot.cs
@@ -7,27 +7,38 @@ public class turnandshoot : MonoBehaviour
     public GameObject bullet;
     public int gunAngle = 0;
     public float RotationSpeed;
+    public float ReloadSpeed;
+    float counter;
     void Start()
     {
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a"))
+        if (ReloadSpeed >= counter)
+        {
+            counter += Time.deltaTime;
+        }
+        if (Input.GetKey("a")|| Input.GetKey("left"))
         {
             transform.Rotate(Vector3.up * - (RotationSpeed * Time.deltaTime));
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
             transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
         }
-        if (Input.GetKeyDown("v"))
+        if (Input.GetKey(KeyCode.Space))
         {
-            Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            GameObject bull = Instantiate(bullet, position, transform.rotation) as GameObject;
-            Rigidbody bullrigidbody = bull.GetComponent<Rigidbody>();
-            bullrigidbody.AddForce(transform.right * 2000);
+            if (ReloadSpeed <= counter)
+            {
+                counter = 0;
+                Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                GameObject bull = Instantiate(bullet, position, transform.rotation) as GameObject;
+                Rigidbody bullrigidbody = bull.GetComponent<Rigidbody>();
+                bullrigidbody.AddForce(transform.right * 2000);
+            }
         }
     }
 }
