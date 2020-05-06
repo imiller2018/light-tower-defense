19ff3435ca3730f849e6e14dedc35cf05eadf7da
joeack99 <55418304+joeack99@users.noreply.github.com>
2020-05-05 20:44:43 -0400

    wall
    walls have holes

---
+++ Code/Tower Defence Protocall/Assets/scripts/main menu/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+   public void BtnNewScene()
+	{
+		SceneManager.LoadScene("Scene 1");
+		Console.Write("Test");
+	}
+}
